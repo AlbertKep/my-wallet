4242538348589255cef80af37b9cbfe3a6a3d358
Albert <albertkepski3@gmail.com>
2025-09-15 10:42:21 +0200

    Import ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { GlobalStyle } from "./assets/styles/GlobalStyles.ts";
+import ThemeProvider from "./ThemeProvider.tsx";
 const App = () => {
   return (
-    <>
+    <ThemeProvider>
+      <GlobalStyle />
       <h1>Hello World</h1>
-    </>
+    </ThemeProvider>
   );
 };
 
