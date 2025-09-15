c777ff3e00b3c4f3bd030cee4241b1ca91ca64ba
Albert <albertkepski3@gmail.com>
2025-09-15 10:36:33 +0200

    Create global styles

---
+++ src/assets/styles/GlobalStyles.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62,5%;
+  }
+
+  body {
+    height: 100svh;
+    margin: 0;
+    font-size: 1.6rem;
+    /* background: #3F5EFB;
+    background: linear-gradient(rgba(63, 94, 251, 1) 10%, rgba(252, 70, 107, 1) 100%); */
+    background: #B134C9;
+    background: linear-gradient(rgba(177, 52, 201, 1) 10%, rgba(252, 70, 107, 1) 100%);
+  }
+
+  li {
+    list-style-type: none;
+  }
+`;
---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    white: "##FFFFFF",
+    orange: "##FF9A31",
+    yellow: "#FFDC18",
+    darkBlue: "#021F40",
+    red: "#FF2031",
+    green: "#0AAB55",
+  },
+
+  font: {
+    family: {
+      montserrat: '"Montserrat", sans-serif',
+    },
+  },
+
+  mq: {
+    tablet: "@media (min-width: 768px)",
+    desktop: "@media (min-width: 1024px)",
+    bigDesktop: "@media (min-width: 1280px)",
+    huge: "@media (min-width: 1440px)",
+  },
+};
