d9073eaa095c8c26fc3207cc9eb0f00e44265b0b
Albert <albertkepski3@gmail.com>
2025-09-16 11:10:26 +0200

    Fix invalid color values and define theme types

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,19 +1,19 @@
-export const theme = {
+import { type DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
   colors: {
-    white: "##FFFFFF",
-    orange: "##FF9A31",
+    white: "#FFFFFF",
+    orange: "#FF9A31",
     yellow: "#FFDC18",
     darkBlue: "#021F40",
     red: "#FF2031",
     green: "#0AAB55",
   },
-
   font: {
     family: {
       montserrat: '"Montserrat", sans-serif',
     },
   },
-
   mq: {
     tablet: "@media (min-width: 768px)",
     desktop: "@media (min-width: 1024px)",
