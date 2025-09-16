4996cb7c07c403a2cbabd9ed1026e4bc7de86279
Albert <albertkepski3@gmail.com>
2025-09-16 11:00:37 +0200

    Add Montserrat font to global styles

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -13,6 +13,7 @@ export const GlobalStyle = createGlobalStyle`
     height: 100svh;
     margin: 0;
     font-size: 1.6rem;
+    font-family: "Montserrat", sans-serif;
     /* background: #3F5EFB;
     background: linear-gradient(rgba(63, 94, 251, 1) 10%, rgba(252, 70, 107, 1) 100%); */
     background: #B134C9;
