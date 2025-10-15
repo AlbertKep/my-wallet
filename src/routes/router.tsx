875246b16f2c9e8620096bb5805835588b444d19
Albert <albertkepski3@gmail.com>
2025-10-15 10:38:13 +0200

    Add Register and SignIn page to route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,7 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "../App.tsx";
+import Register from "../pages/register/Register.tsx";
+import SignIn from "../pages/signIn/SignIn.tsx";
 import Dashboard from "../pages/dashboard/Dashboard.tsx";
 import Statistics from "../pages/statistics/Statistics.tsx";
 import Transactions from "../pages/transactions/Transactions.tsx";
@@ -11,6 +9,8 @@ import Transactions from "../pages/transactions/Transactions.tsx";
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route path="/register" element={<Register />} />
+      <Route path="/signin" element={<SignIn />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/statistics" element={<Statistics />} />
       <Route path="/transactions" element={<Transactions />} />
