13a1a9e31db86a1c5fb5e9b0c8b7a786cba20ff7
Albert <albertkepski3@gmail.com>
2025-12-09 10:41:39 +0100

    Implement signInUser for login

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,10 @@
 import { auth } from "./firebase.ts";
-import { createUserWithEmailAndPassword, type User } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, type User, type UserCredential } from "firebase/auth";
+
+type SignInResult = {
+  user?: UserCredential;
+  authError?: string;
+};
 
 export const registerUser = async (email: string, password: string): Promise<User | null> => {
   try {
@@ -13,5 +18,18 @@ export const registerUser = async (email: string, password: string): Promise<Use
       console.error("Unexpected error", error);
     }
     return null;
+  }
+};
+
+export const signInUser = async (email: string, password: string): Promise<SignInResult> => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return { user: userCredential };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { authError: "Invalid email or Password" };
+    } else {
+      return { authError: "Unexpected error occurred" };
+    }
   }
 };
