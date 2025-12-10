2f4135baeb82e6d325b697ad012ff009db637cc4
Albert <albertkepski3@gmail.com>
2025-12-10 08:47:20 +0100

    Accept handleSubmit function as prop

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,14 +1,14 @@
-import type { ReactNode } from "react";
+import type { ReactNode, FormEvent } from "react";
 import { StyledForm, BackgroundWrapper, StyledButton, AuthLinkHint } from "./Form.ts";
 
 type FormProps = {
   children: ReactNode;
+  handleSubmit: (event: FormEvent) => Promise<void>;
 };
-const Form: React.FC<FormProps> = ({ children }) => {
+const Form: React.FC<FormProps> = ({ children, handleSubmit }) => {
   return (
     <>
-      {/* <StyledForm onSubmit={handleSubmit}> */}
-      <StyledForm>
+      <StyledForm onSubmit={handleSubmit}>
         <BackgroundWrapper>{children}</BackgroundWrapper>
         <StyledButton>Register</StyledButton>
       </StyledForm>
