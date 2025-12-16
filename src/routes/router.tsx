bfbf547dd9f94e13c7b891e914aca19464f9976f
Albert <albertkepski3@gmail.com>
2025-12-16 11:42:04 +0100

    Restructure routes with separate Public and Protected layouts

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "../App.tsx";
+import ProtectedLayout from "../components/layout/ProtectedLayout.tsx";
+import PublicLayout from "../components/layout/PublicLayout.tsx";
 import Register from "../pages/register/Register.tsx";
 import SignIn from "../pages/signIn/SignIn.tsx";
 import Dashboard from "../pages/dashboard/Dashboard.tsx";
@@ -8,12 +10,19 @@ import Transactions from "../pages/transactions/Transactions.tsx";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="/register" element={<Register />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/statistics" element={<Statistics />} />
-      <Route path="/transactions" element={<Transactions />} />
+    <Route path='/' element={<App />}>
+      {/* Public routes */}
+      <Route element={<PublicLayout />}>
+        <Route path='/' element={<Register />} />
+        <Route path='signin' element={<SignIn />} />
+      </Route>
+
+      {/* Protected routes */}
+      <Route element={<ProtectedLayout />}>
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='statistics' element={<Statistics />} />
+        <Route path='transactions' element={<Transactions />} />
+      </Route>
     </Route>
   )
 );
