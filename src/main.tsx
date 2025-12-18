aab2f6ef26ba98d704b54eebaa7fed6447a33b8a
Albert <albertkepski3@gmail.com>
2025-12-18 09:28:39 +0100

    Wrap app with AuthProvider and RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from "react-dom/client";
+import * as React from "react";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/router";
+import { router } from "./routes/router.tsx";
+import { AuthProvider } from "./context/AuthProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
 );
