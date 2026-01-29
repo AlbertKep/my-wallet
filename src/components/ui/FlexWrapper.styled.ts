2b6552cac6ff081d27abbe8caf7c205f69a76f1d
Albert <albertkepski3@gmail.com>
2026-01-29 10:17:30 +0100

    Add FlexWrapper styled component for centered layout

---
+++ src/components/ui/FlexWrapper.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FlexWrapper = styled.div`
+  height: 90svh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
