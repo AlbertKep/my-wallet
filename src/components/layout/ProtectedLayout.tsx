1063956adffaf0a64bf6fc4523874c2b7f384f4b
Albert <albertkepski3@gmail.com>
2026-01-29 10:53:51 +0100

    Wrap Navbar and Outlet in LayoutWrapper to enable responsive desktop layout

--- src/components/layout/ProtectedLayout.tsx
+++ src/components/layout/ProtectedLayout.tsx
@@ -2,19 +2,21 @@ import { Navigate, Outlet } from "react-router-dom";
 import Header from "../header/Header.tsx";
 import Navbar from "../navbar/Navbar.tsx";
 import { useAuth } from "../../context/AuthContext.ts";
+import { LayoutWrapper } from "../ui/LayoutWrapper.styled.ts";
 
 const ProtectedLayout = () => {
   const { user, loading } = useAuth();
-  console.log("ProtectedLayout:", { user, loading });
 
   if (loading) return <p>Loading...</p>;
   if (!user) return <Navigate to='/signin' replace />;
   return (
-    <main>
+    <>
       <Header />
-      <Navbar />
-      <Outlet />
-    </main>
+      <LayoutWrapper>
+        <Navbar />
+        <Outlet />
+      </LayoutWrapper>
+    </>
   );
 };
 
