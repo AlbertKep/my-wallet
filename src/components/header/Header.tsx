d6d073e0ad32a364635153362b69a2cd52b84100
Albert <albertkepski3@gmail.com>
2026-01-29 09:46:06 +0100

    Clean up unused imports

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext.ts";
 import Logo from "../logo/Logo.tsx";
 import { Button } from "../ui/Button.styled.ts";
-import { StyledHeader, Wrapper, Heading, ButtonWrapper } from "./Header.styled.ts";
+import { StyledHeader, ButtonWrapper } from "./Header.styled.ts";
 
 const Header = () => {
   console.log("Header mounted");
