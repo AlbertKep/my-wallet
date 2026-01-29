7f4f14a17a929b627771f87d668a3fe19a7f9b54
Albert <albertkepski3@gmail.com>
2026-01-29 09:45:24 +0100

    Remove inline logo markup and clean up unused imports

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,6 @@
-import wallet from "../../assets/icons/wallet.svg";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext.ts";
+import Logo from "../logo/Logo.tsx";
 import { Button } from "../ui/Button.styled.ts";
 import { StyledHeader, Wrapper, Heading, ButtonWrapper } from "./Header.styled.ts";
 
