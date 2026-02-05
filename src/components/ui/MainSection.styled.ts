5782476b0308b2cb7823b213142445daadd5c0b3
Albert <albertkepski3@gmail.com>
2026-02-05 10:51:38 +0100

    Add desktop layout and responsive height to MainSection

--- src/components/ui/MainSection.styled.ts
+++ src/components/ui/MainSection.styled.ts
@@ -2,4 +2,10 @@ import styled from "styled-components";
 
 export const MainSection = styled.section`
   flex: 4;
+  padding-bottom: 1rem;
+
+  ${({ theme }) => theme.mq.desktop} {
+    display: flex;
+    height: clamp(400px, 70vh, 1000px);
+  }
 `;
