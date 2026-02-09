d265a27b1c971d4b9c4f97c47ed514f7db1b9fc6
Albert <albertkepski3@gmail.com>
2026-02-09 11:56:06 +0100

    Update AuthProvider import path after folder restructure

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import ReactDOM from "react-dom/client";
 import * as React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.tsx";
-import { AuthProvider } from "./context/AuthProvider.tsx";
+import { AuthProvider } from "./context/auth/AuthProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
