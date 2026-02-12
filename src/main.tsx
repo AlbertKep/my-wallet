d5cacc19654a590fbe651ae22742800869613386
Albert <albertkepski3@gmail.com>
2026-02-12 12:21:51 +0100

    Integrate TransactionsProvider into app root to expose transaction data globally

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import * as React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.tsx";
 import { AuthProvider } from "./context/auth/AuthProvider.tsx";
+import { TransactionsProvider } from "./context/transactions/TransactionsProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <TransactionsProvider>
+        <RouterProvider router={router} />
+      </TransactionsProvider>
     </AuthProvider>
   </React.StrictMode>,
 );
