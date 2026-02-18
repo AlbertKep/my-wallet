ec45452624c02152c792225cc950f68ea6d0b156
Albert <albertkepski3@gmail.com>
2026-02-18 10:47:57 +0100

    Integrate Dashboard with transactions context and remove static TransactionList usage

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,16 @@
 import { MainSection } from "../../components/ui/MainSection.styled.ts";
 import BalanceSummary from "../../components/balanceSummary/BalanceSummary.tsx";
 import TransactionList from "../../components/transactionList/TransactionList.tsx";
+import { useTransactions } from "../../context/transactions/TransactionsContext.ts";
 
 const Dashboard = () => {
+  const { transactions, loading } = useTransactions();
+
+  if (loading) return <p>Loading...</p>;
   return (
     <MainSection>
       <BalanceSummary />
-      <TransactionList />
+      <TransactionList transactions={transactions} />
     </MainSection>
   );
 };
