14456575e0f35d4d57806d300bc4d94f118d2d72
Albert <albertkepski3@gmail.com>
2026-02-18 10:44:53 +0100

    Integrate TransactionList with real data and proper typing

--- src/components/transactionList/TransactionList.tsx
+++ src/components/transactionList/TransactionList.tsx
@@ -2,23 +2,13 @@ import { useNavigate } from "react-router-dom";
 import { Wrapper, Heading, ButtonWrapper } from "./TransactionList.styled.ts";
 import TransactionItem from "../transactionItem/TransactionItem.tsx";
 import { Button } from "../ui/Button.styled.ts";
+import { type TransactionWithId } from "../../services/transactions.ts";
 
-// transaction icons
-import entertainment from "../../assets/icons/transactionIcons/entertainment.svg";
-import food from "../../assets/icons/transactionIcons/food.svg";
-import salary from "../../assets/icons/transactionIcons/salary.svg";
-// import transport from "../../assets/icons/transactionIcons/transport.svg";
+type TransactionListProps = {
+  transactions: TransactionWithId[];
+};
 
-const transactions = [
-  { id: 1, icon: food, alt: "Food icon", title: "Biedronka", category: "Groceries", price: -54.2, date: "2026-01-12" },
-  { id: 2, icon: salary, alt: "Salary icon", title: "Wypłata", category: "Income", price: 4200, date: "2026-01-10" },
-  { id: 3, icon: entertainment, alt: "Entertainment icon", title: "Netflix", category: "Subscription", price: -29, date: "2026-01-08" },
-  { id: 4, icon: entertainment, alt: "Entertainment icon", title: "Netflix", category: "Subscription", price: -29, date: "2026-01-08" },
-  { id: 5, icon: entertainment, alt: "Entertainment icon", title: "Netflix", category: "Subscription", price: -29, date: "2026-01-08" },
-  // { id: 3, icon: entertainment, alt: "Entertainment icon", title: "Netflix", category: "Subscription", price: -29, date: "2026-01-08" },
-];
-
-const TransactionList = () => {
+const TransactionList: React.FC<TransactionListProps> = ({ transactions }) => {
   const navigate = useNavigate();
 
   return (
@@ -26,7 +16,7 @@ const TransactionList = () => {
       <Heading>Last transactions</Heading>
       <ul>
         {transactions?.map((transaction) => (
-          <TransactionItem key={transaction.id} {...transaction} />
+          <TransactionItem key={transaction.transactionID} {...transaction} />
         ))}
       </ul>
       <ButtonWrapper>
