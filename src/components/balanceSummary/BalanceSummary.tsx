82b5f20b0e42340cd1bd5fedbad1f72af8ebc69e
Albert <albertkepski3@gmail.com>
2026-02-19 11:35:16 +0100

    Add balance summary calculation using reduce

--- src/components/balanceSummary/BalanceSummary.tsx
+++ src/components/balanceSummary/BalanceSummary.tsx
@@ -1,12 +1,35 @@
 import balanceChart from "../../assets/icons/balance_chart.svg";
 import { Wrapper, TextWrapper, Heading, Balance, ImgWrapper } from "./BalanceSummary.styled.ts";
+import { type TransactionWithId } from "../../services/transactions.ts";
+import { useState, useEffect } from "react";
 
-const BalanceSummary = () => {
+type TransactionListProps = {
+  transactions: TransactionWithId[];
+};
+
+const BalanceSummary: React.FC<TransactionListProps> = ({ transactions }) => {
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    const getBalance = () => {
+      const { income, expense } = transactions.reduce(
+        (total: { income: number; expense: number }, currentValue) => {
+          if (currentValue.type === "income") total.income += currentValue.price;
+          if (currentValue.type === "expense") total.expense += currentValue.price;
+          return total;
+        },
+        { income: 0, expense: 0 },
+      );
+      setBalance(income - expense);
+    };
+    getBalance();
+  }, [transactions]);
+
   return (
     <Wrapper>
       <TextWrapper>
         <Heading>Main Balance</Heading>
-        <Balance>3200 zł</Balance>
+        <Balance>{balance} zł</Balance>
       </TextWrapper>
 
       <ImgWrapper>
