ef3e4bf07ecb41dc52604b49c724bbd79b3a43e6
Albert <albertkepski3@gmail.com>
2026-02-26 12:39:33 +0100

    Add AddTransaction route and component import

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -7,6 +7,7 @@ import SignIn from "../pages/signIn/SignIn.tsx";
 import Dashboard from "../pages/dashboard/Dashboard.tsx";
 import Statistics from "../pages/statistics/Statistics.tsx";
 import Transactions from "../pages/transactions/Transactions.tsx";
+import AddTransaction from "../pages/addTransaction/AddTransaction.tsx";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,7 +24,8 @@ export const router = createBrowserRouter(
         <Route path='dashboard' element={<Dashboard />} />
         <Route path='statistics' element={<Statistics />} />
         <Route path='transactions' element={<Transactions />} />
+        <Route path='add' element={<AddTransaction />} />
       </Route>
-    </Route>
-  )
+    </Route>,
+  ),
 );
