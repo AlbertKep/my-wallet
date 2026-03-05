402c3d9d70c23d67814574ea9d1e2c95732196c9
Albert <albertkepski3@gmail.com>
2026-03-05 11:42:00 +0100

    Add TransactionTypeToggle component for selecting transaction type

---
+++ src/components/transactionTypeToggle/TransactionTypeToggle.tsx
@@ -0,0 +1,25 @@
+import { TypeWrapper, TypeOption } from "./TransactionTypeToogle.styled.ts";
+import { type TransactionFieldUpdate } from "../../pages/addTransaction/AddTransaction.tsx";
+
+type TransactionTypeToggleProps = {
+  selectedType: string;
+  updateField: ({ field, value }: TransactionFieldUpdate) => void;
+};
+const TransactionTypeToggle: React.FC<TransactionTypeToggleProps> = ({ selectedType, updateField }) => {
+  const isExpense = selectedType === "expense";
+
+  const handleChooseType = (type: string) => updateField({ field: "type", value: type });
+
+  return (
+    <TypeWrapper>
+      <TypeOption $active={isExpense} onClick={() => handleChooseType("expense")}>
+        Expense
+      </TypeOption>
+      <TypeOption $active={!isExpense} onClick={() => handleChooseType("income")}>
+        Income
+      </TypeOption>
+    </TypeWrapper>
+  );
+};
+
+export default TransactionTypeToggle;
