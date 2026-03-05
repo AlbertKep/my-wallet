e7f7f122055b830283cb5502d49ad29764a7aadd
Albert <albertkepski3@gmail.com>
2026-03-05 10:58:45 +0100

    Add timestamp/input date converters and fix date formatting

---
+++ src/utils/dateConverters.ts
@@ -0,0 +1,29 @@
+import { Timestamp } from "firebase/firestore";
+
+const formatter = new Intl.DateTimeFormat("pl-PL", {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "numeric",
+});
+
+export const formatDate = (seconds: number) => {
+  const date = new Date(seconds * 1000);
+  return formatter.format(date) ?? "-";
+};
+
+export const timestampToInputDate = (timestamp: Timestamp) => {
+  const date = timestamp.toDate();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}-${month < 10 ? "0" + month : month}-${day < 10 ? "0" + day : day}`;
+};
+
+export const inputDateToTimestamp = (dateString: string) => {
+  const date = new Date(dateString);
+  console.log(Timestamp.fromDate(date));
+  return Timestamp.fromDate(date);
+};
