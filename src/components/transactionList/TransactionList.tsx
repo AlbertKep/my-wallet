1cb27b25d2f83207466331bdb17ef829f1ce5fad
Albert <albertkepski3@gmail.com>
2026-03-05 11:19:27 +0100

    Update layout wrapper, use shared Heading and add navigation to buttons

--- src/components/transactionList/TransactionList.tsx
+++ src/components/transactionList/TransactionList.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import { Wrapper, Heading, ButtonWrapper } from "./TransactionList.styled.ts";
+import { StyledSectionWrapper, ButtonWrapper } from "./TransactionList.styled.ts";
+import { Heading } from "../ui/Heading.styled.ts";
 import TransactionItem from "../transactionItem/TransactionItem.tsx";
 import { Button } from "../ui/Button.styled.ts";
 import { type TransactionWithId } from "../../services/transactions.ts";
@@ -12,7 +13,7 @@ const TransactionList: React.FC<TransactionListProps> = ({ transactions }) => {
   const navigate = useNavigate();
 
   return (
-    <Wrapper>
+    <StyledSectionWrapper>
       <Heading>Last transactions</Heading>
       <ul>
         {transactions?.map((transaction) => (
@@ -20,10 +21,10 @@ const TransactionList: React.FC<TransactionListProps> = ({ transactions }) => {
         ))}
       </ul>
       <ButtonWrapper>
-        <Button>Add Transaction</Button>
+        <Button onClick={() => navigate("/add")}>Add Transaction</Button>
         <Button onClick={() => navigate("/transactions")}>All Transactions</Button>
       </ButtonWrapper>
-    </Wrapper>
+    </StyledSectionWrapper>
   );
 };
 
