f76b67a5863d4fcca7c9ee105f7cbecd9732701e
Albert <albertkepski3@gmail.com>
2026-03-05 11:28:33 +0100

    Remove local Heading styles after moving to shared UI

--- src/components/transactionList/TransactionList.styled.ts
+++ src/components/transactionList/TransactionList.styled.ts
@@ -16,13 +16,6 @@ export const Wrapper = styled.div`
   }
 `;
 
-export const Heading = styled.h3`
-  font-size: clamp(1.25rem, 1rem + 1.5vw, 2rem);
-  color: ${({ theme }) => theme.colors.darkBlue};
-  font-weight: 600;
-  padding: 0.5em;
-`;
-
 export const ButtonWrapper = styled.div`
   padding-bottom: 0.5em;
   text-align: center;
