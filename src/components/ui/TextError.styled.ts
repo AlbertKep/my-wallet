1f03ea557edcf0183be905318097f9bdb9fba04b
Albert <albertkepski3@gmail.com>
2026-03-11 11:26:11 +0100

    Improve TextError spacing and consistent height

--- src/components/ui/TextError.styled.ts
+++ src/components/ui/TextError.styled.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const TextError = styled.p<{ $visible?: boolean }>`
-  min-height: 1em;
+  min-height: 1rem;
+  margin: 0.5em 0;
   color: ${({ theme }) => theme.colors.red};
   font-size: clamp(0.6rem, 0.6rem + 0.5vw, 1rem);
   font-weight: 600;
