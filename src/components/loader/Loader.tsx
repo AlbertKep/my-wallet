2f51de9a5582d62345fd8577cf7aefde710dd128
Albert <albertkepski3@gmail.com>
2026-03-19 11:18:30 +0100

    Add Loader component with styles

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,11 @@
+import { FlexWrapper } from "../ui/FlexWrapper.styled";
+import { StyledLoader } from "./Loader.styled.ts";
+const Loader = () => {
+  return (
+    <FlexWrapper>
+      <StyledLoader />
+    </FlexWrapper>
+  );
+};
+
+export default Loader;
