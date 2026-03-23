f614a4499db137e1a901bd33f27116891b13739f
Albert <albertkepski3@gmail.com>
2026-03-23 20:53:50 +0100

    Add custom showToast utility using Toast component

---
+++ src/utils/showToast.tsx
@@ -0,0 +1,7 @@
+import toast from "react-hot-toast";
+import Toast from "../components/toast/Toast.tsx";
+import type { Toast as ToastType } from "react-hot-toast";
+
+export const showToast = (message: string, icon: string) => {
+  toast.custom((t: ToastType) => <Toast t={t} message={message} icon={icon} />, { duration: 1000 });
+};
