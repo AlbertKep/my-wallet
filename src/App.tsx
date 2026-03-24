e965297e06cdda4ccd37cbfa1232e6715dfb98ba
Albert <albertkepski3@gmail.com>
2026-03-24 10:42:20 +0100

    Integrate react-hot-toast Toaster in App root

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { GlobalStyle } from "./assets/styles/GlobalStyles.ts";
 import Theme from "./ThemeProvider.tsx";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <Theme>
       <GlobalStyle />
+      <Toaster />
       <Outlet />
     </Theme>
   );
