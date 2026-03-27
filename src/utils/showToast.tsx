3a8e5b044f1c8fbb42b4ff06ef5174633766893e
Albert <albertkepski3@gmail.com>
2026-03-27 10:33:11 +0100

    Increase toast duration from 1000ms to 2000ms

--- src/utils/showToast.tsx
+++ src/utils/showToast.tsx
@@ -3,5 +3,5 @@ import Toast from "../components/toast/Toast.tsx";
 import type { Toast as ToastType } from "react-hot-toast";
 
 export const showToast = (message: string, icon: string) => {
-  toast.custom((t: ToastType) => <Toast t={t} message={message} icon={icon} />, { duration: 1000 });
+  toast.custom((t: ToastType) => <Toast t={t} message={message} icon={icon} />, { duration: 2000 });
 };
