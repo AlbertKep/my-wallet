49287e3475d9c2c131caa422c137e44e82a41cb6
Albert <albertkepski3@gmail.com>
2026-04-01 11:23:05 +0200

    Hide Navbar when no transactions are available

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,26 +2,31 @@ import house from "../../assets/icons/house.svg";
 import statistic from "../../assets/icons/statistic.svg";
 import transaction from "../../assets/icons/transaction.svg";
 import { Navigation, List, Item, StyledNavLink } from "./Navbar.styled.ts";
+import { useTransactions } from "../../context/transactions/TransactionsContext.ts";
 
 const Navbar = () => {
+  const { transactions } = useTransactions();
+
+  if (transactions.length === 0) return null;
+
   return (
     <Navigation>
       <List>
         <Item>
-          <StyledNavLink to="/">
-            <img src={house} alt="house icon" />
+          <StyledNavLink to='/dashboard'>
+            <img src={house} alt='house icon' />
             <span>Dashboard</span>
           </StyledNavLink>
         </Item>
         <Item>
-          <StyledNavLink to="/statistics">
-            <img src={statistic} alt="statistic icon" />
+          <StyledNavLink to='/statistics'>
+            <img src={statistic} alt='statistic icon' />
             <span>Statistic</span>
           </StyledNavLink>
         </Item>
         <Item>
-          <StyledNavLink to="/transactions">
-            <img src={transaction} alt="transaction icon" />
+          <StyledNavLink to='/transactions'>
+            <img src={transaction} alt='transaction icon' />
             <span>Transaction</span>
           </StyledNavLink>
         </Item>
