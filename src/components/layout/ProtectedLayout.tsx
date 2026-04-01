cd43cb441d030825d2d8b4e3437724e6c002026c
Albert <albertkepski3@gmail.com>
2026-04-01 11:15:58 +0200

    Adjust ProtectedLayout to pass  based on transactions

--- src/components/layout/ProtectedLayout.tsx
+++ src/components/layout/ProtectedLayout.tsx
@@ -2,18 +2,20 @@ import { Navigate, Outlet } from "react-router-dom";
 import Header from "../header/Header.tsx";
 import Navbar from "../navbar/Navbar.tsx";
 import { useAuth } from "../../context/auth/AuthContext.ts";
+import { useTransactions } from "../../context/transactions/TransactionsContext.ts";
 import { LayoutWrapper } from "../ui/LayoutWrapper.styled.ts";
 import Loader from "../loader/Loader.tsx";
 
 const ProtectedLayout = () => {
   const { user, loading } = useAuth();
+  const { transactions } = useTransactions();
 
   if (loading) return <Loader />;
   if (!user) return <Navigate to='/signin' replace />;
   return (
     <>
       <Header />
-      <LayoutWrapper>
+      <LayoutWrapper $hasNavbar={transactions.length !== 0}>
         <Outlet />
         <Navbar />
       </LayoutWrapper>
