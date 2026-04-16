ffc6f082717a2a9afa06b512579836cbb115ae40
Albert <albertkepski3@gmail.com>
2026-04-16 13:07:33 +0200

    Adjust border-radius from 20px to 12px

--- src/components/ui/Button.styled.ts
+++ src/components/ui/Button.styled.ts
@@ -7,6 +7,6 @@ export const Button = styled.button`
   color: ${({ theme }) => theme.colors.white};
   font-size: clamp(0.6rem, 0.8rem + 1vw, 1.5rem);
   font-weight: 600;
-  border-radius: 20px;
+  border-radius: 12px;
   padding: 0.5em 1em;
 `;
