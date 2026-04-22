c208014a097c155c9bcb1f13a20da7a19d2eceac
Albert <albertkepski3@gmail.com>
2026-04-22 08:54:00 +0200

    Add layout wrappers and integrate MainSection/Surface styling

---
+++ src/pages/transactions/Transactions.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { MainSection } from "../../components/ui/MainSection.styled.ts";
+import { Surface } from "../../components/ui/Surface.styled.ts";
+
+export const StyledMainSection = styled(MainSection)`
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: space-between;
+  position: relative;
+  padding: 0 0.25em;
+`;
+
+export const TransactionWrapper = styled(Surface)``;
