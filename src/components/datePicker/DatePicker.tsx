affeee7552b36d8c19ec0db08dab2cfa5692e000
Albert <albertkepski3@gmail.com>
2026-04-28 11:13:04 +0200

    Support optional error prop and render TextError

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -2,14 +2,16 @@ import { DateWrapper, DateInput } from "./DatePicker.styled.ts";
 import { Label } from "../ui/Label.styled.ts";
 import { timestampToInputDate, inputDateToTimestamp } from "../../utils/dateConverters.ts";
 import type { Timestamp } from "firebase/firestore";
+import { TextError } from "../ui/TextError.styled.ts";
 
 type DatePickerProps<T> = {
   id: string;
   label: string;
   selectedDate: Timestamp | null;
+  error?: string;
   updateField: (payload: T) => void;
 };
-const DatePicker = <T,>({ id, label, selectedDate, updateField }: DatePickerProps<T>) => {
+const DatePicker = <T,>({ id, label, selectedDate, error, updateField }: DatePickerProps<T>) => {
   return (
     <DateWrapper>
       <Label htmlFor={id}>{label}</Label>
@@ -20,6 +22,7 @@ const DatePicker = <T,>({ id, label, selectedDate, updateField }: DatePickerProp
         value={selectedDate === null ? "" : timestampToInputDate(selectedDate)}
         onChange={(e) => updateField({ field: id, value: inputDateToTimestamp(e.target.value) } as T)}
       />
+      <TextError $visible={!!error}>{error}</TextError>
     </DateWrapper>
   );
 };
