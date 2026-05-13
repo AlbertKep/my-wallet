04f06614bf2a870e377baa624c22ab0fd750444c
Albert <albertkepski3@gmail.com>
2026-05-13 13:56:26 +0200

    Update component with alias imports and scrollable wrapper

--- src/pages/transactions/Transactions.styled.ts
+++ src/pages/transactions/Transactions.styled.ts
@@ -1,13 +1,16 @@
 import styled from "styled-components";
-import { MainSection } from "../../components/ui/MainSection.styled.ts";
-import { Surface } from "../../components/ui/Surface.styled.ts";
+import { MainSection } from "@/components/ui/MainSection.styled.ts";
+import { Surface } from "@/components/ui/Surface.styled.ts";
 
 export const StyledMainSection = styled(MainSection)`
-  flex-wrap: wrap;
   flex-direction: column;
   justify-content: space-between;
   position: relative;
   padding: 0 0.25em;
 `;
 
-export const TransactionWrapper = styled(Surface)``;
+export const TransactionWrapper = styled(Surface)`
+  max-height: 500px;
+  overflow-y: auto;
+  padding: 0.5em;
+`;
