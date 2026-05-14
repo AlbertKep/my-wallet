8c06475801eb921642873e91298bc86ccae5be13
Albert <albertkepski3@gmail.com>
2026-05-14 09:44:29 +0200

    update component with scroll wrapper, buttons and layout fixes

--- src/pages/transactions/components/filters/Filters.styled.ts
+++ src/pages/transactions/components/filters/Filters.styled.ts
@@ -1,22 +1,38 @@
 import styled from "styled-components";
 import { Surface } from "@/components/ui/Surface.styled.ts";
 import { Label } from "@/components/ui/Label.styled.ts";
+import { Button } from "@/components/ui/Button.styled";
+// animation
+import { dropdownMotion } from "@/assets/styles/animations";
 
-export const Wrapper = styled(Surface)`
+export const Wrapper = styled(Surface)<{ $isOpen: boolean }>`
+  position: absolute;
+  /* top: 105%; */
+  left: 0.25em;
+  right: 0.25em;
+  z-index: 2;
+  width: auto;
+  padding: 0.75em;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.08);
+
+  ${({ theme }) => theme.mq.desktop} {
+    top: 15%;
+  }
+  ${dropdownMotion}
+`;
+
+export const ScrollWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: flex-start;
   justify-content: space-around;
   gap: 1em;
-  position: absolute;
-  top: 80px;
-  left: 0.25em;
-  right: 0.25em;
-  z-index: 2;
-  width: auto;
-  padding: 0.5em;
+  height: 60vh;
+  overflow-y: auto;
 
   ${({ theme }) => theme.mq.tablet} {
+    height: auto;
+    overflow-y: visible;
     gap: 1.5em;
   }
 `;
@@ -31,7 +47,7 @@ export const FilterGroup = styled.div<{ $variant?: string }>`
   ${({ theme }) => theme.mq.tablet} {
     display: flex;
     flex-direction: column;
-    gap: ${({ $variant }) => ($variant === "dates" ? "1.3em" : $variant === "category" ? "1.5em" : $variant === "range" ? "0" : "0.75em")};
+    gap: ${({ $variant }) => ($variant === "dates" ? "0.1em" : $variant === "category" ? "1.5em" : $variant === "range" ? "0" : "0.75em")};
     flex: 1;
 
     & div {
@@ -47,4 +63,14 @@ export const StyledLabel = styled(Label)`
   ${({ theme }) => theme.mq.tablet} {
     margin-bottom: 0;
   }
+`;
+
+export const StyledButton = styled(Button)`
+  font-size: clamp(0.9rem, 2.2vw, 1.1rem);
+  margin-right: 0.5em;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  text-align: right;
 `;
