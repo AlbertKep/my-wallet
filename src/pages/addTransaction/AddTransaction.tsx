25d02d8265610f56945495842e44dcaf30815bd5
Albert <albertkepski3@gmail.com>
2026-05-25 09:47:09 +0200

    Reorganize imports and update form components

--- src/pages/addTransaction/AddTransaction.tsx
+++ src/pages/addTransaction/AddTransaction.tsx
@@ -1,21 +1,28 @@
 import { useState, useEffect, type FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import { MainSection } from "../../components/ui/MainSection.styled.ts";
-import { SectionWrapper } from "../../components/ui/SectionWrapper.styled.ts";
-import { Heading } from "../../components/ui/Heading.styled.ts";
-import { ContentWrapper, FormWrapper, TransactionFormField, PriceFormField, StyledButton } from "./AddTransaction.styled.ts";
-import DropdownCategory from "../../components/dropdownCategory/DropdownCategory.tsx";
-import TransactionTypeToggle from "../../components/transactionTypeToggle/TransactionTypeToggle.tsx";
-import DatePicker from "../../components/datePicker/DatePicker.tsx";
-import { categories } from "../../data/categoriesData.ts";
-
+// firebase
 import { Timestamp } from "firebase/firestore";
-import { useAuth } from "../../context/auth/AuthContext.ts";
-import { addTransaction } from "../../services/transactions.ts";
-import { transactionValidate, type TransactionErrors } from "../../utils/validation/transactionValidate.ts";
-import { getCategoryIcon } from "../../utils/getCategoryIcon.ts";
-import { showToast } from "../../utils/showToast.tsx";
-import Loader from "../../components/loader/Loader.tsx";
+// styles
+import { MainSection } from "@/components/ui/MainSection.styled.ts";
+import { SectionWrapper } from "@/components/ui/SectionWrapper.styled.ts";
+import { Heading } from "@/components/ui/Heading.styled.ts";
+import { ContentWrapper, FormWrapper, TransactionFormField, PriceFormField, StyledButton } from "./AddTransaction.styled.ts";
+// components
+import DropdownCategory from "@/components/dropdownCategory/DropdownCategory.tsx";
+import TransactionTypeToggle from "@/components/transactionTypeToggle/TransactionTypeToggle.tsx";
+import DatePicker from "@/components/datePicker/DatePicker.tsx";
+import Loader from "@/components/loader/Loader.tsx";
+// utils
+import { transactionValidate, type TransactionErrors } from "@/utils/validation/transactionValidate.ts";
+import { getCategoryIcon } from "@/utils/getCategoryIcon.ts";
+import { showToast } from "@/utils/showToast.tsx";
+// services
+import { addTransaction } from "@/services/transactions.ts";
+// context
+import { useAuth } from "@/context/auth/AuthContext.ts";
+
+import { categories } from "@/data/categoriesData.ts";
+import { transactionTypes } from "@/data/transactionTypesData.ts";
 
 export type TransactionFieldUpdate = {
   field: keyof TransactionForm;
@@ -96,8 +103,8 @@ const AddTransaction = () => {
               error={errors?.price}
             />
             <DropdownCategory categories={categories} selectedCategory={transaction.category} updateField={updateField} />
-            <TransactionTypeToggle selectedType={transaction.type} updateField={updateField} />
-            <DatePicker selectedDate={transaction.date} updateField={updateField} />
+            <TransactionTypeToggle types={transactionTypes} selectedType={transaction.type} updateField={updateField} />
+            <DatePicker id='date' label='Date' selectedDate={transaction.date} updateField={updateField} />
 
             <StyledButton>Add Transaction</StyledButton>
           </FormWrapper>
