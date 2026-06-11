1fbb22ba2c7a234b1eaea4b88d7bd7f9fb54d8dc
Albert <albertkepski3@gmail.com>
2026-06-11 12:01:00 +0200

    Reorder providers and wrap TransactionsProvider with StatsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.tsx";
 import { AuthProvider } from "./context/auth/AuthProvider.tsx";
 import { TransactionsProvider } from "./context/transactions/TransactionsProvider.tsx";
+import { StatsProvider } from "./context/stats/StatsProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <TransactionsProvider>
-        <RouterProvider router={router} />
-      </TransactionsProvider>
+      <StatsProvider>
+        <TransactionsProvider>
+          <RouterProvider router={router} />
+        </TransactionsProvider>
+      </StatsProvider>
     </AuthProvider>
   </React.StrictMode>,
 );
