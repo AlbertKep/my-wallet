b564e0621e5338039c7dc780fa6491674ffa1ec0
Albert <albertkepski3@gmail.com>
2026-06-17 12:19:35 +0200

    Integrate stats into Dashboard and unify loading states

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -6,19 +6,23 @@ import Loader from "@/components/loader/Loader.tsx";
 import EmptyWallet from "@/components/emptyWallet/EmptyWallet.tsx";
 import DashboardTransactionList from "@/components/dashboardTransactionList/DashboardTransactionList.tsx";
 
+// context
 import { useTransactions } from "@/context/transactions/TransactionsContext.ts";
+import { useStats } from "@/context/stats/StatsContext.ts";
 
 const Dashboard = () => {
-  const { transactions, loading } = useTransactions();
-  const isEmpty = transactions.length === 0;
-  if (loading) return <Loader />;
+  const { transactions, loading: transactionLoading } = useTransactions();
+  const { stats, loading: statsLoading } = useStats();
+
+  const isEmpty = transactions.length === 0 && stats === null;
+  if (transactionLoading || statsLoading) return <Loader />;
   return (
     <MainSection>
       {isEmpty ? (
         <EmptyWallet />
       ) : (
         <>
-          <BalanceSummary transactions={transactions} />
+          <BalanceSummary {...stats} />
           <DashboardTransactionList transactions={transactions.slice(0, 5)} />
         </>
       )}
