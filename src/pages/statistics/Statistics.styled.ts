679a33fe1bdc19c351ad9a76a3cd450c674c1b1d
Albert <albertkepski3@gmail.com>
2026-07-15 20:52:28 +0200

    Add Statistics page with grid layout and inline sections

---
+++ src/pages/statistics/Statistics.styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StatisticsWrapper = styled.div`
+  overflow-y: auto;
+  max-height: 480px;
+  ${({ theme }) => theme.mq.tablet} {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: minmax(0, auto);
+    gap: 1em;
+    grid-template-areas:
+      "balance balance balance"
+      "chart chart chart"
+      "category-breakdown category-breakdown other";
+  }
+`;
--- src/pages/statistics/Statistics.tsx
+++ src/pages/statistics/Statistics.tsx
@@ -1,5 +1,52 @@
+import styled from "styled-components";
+// styles
+import { Heading } from "@/components/ui/Heading.styled";
+import { MainSection } from "@/components/ui/MainSection.styled";
+import { SectionWrapper } from "@/components/ui/SectionWrapper.styled";
+import { StatisticsWrapper } from "./Statistics.styled";
+// components
+import Loader from "@/components/loader/Loader";
+import BalanceList from "./components/balanceList/BalanceList";
+import CategoryBreakdown from "./components/categoryBreakdown/CategoryBreakdown";
+// context
+import { useStats } from "@/context/stats/StatsContext";
+
+const Chart = styled.div`
+  background-color: red;
+  grid-area: chart;
+`;
+
+const OtherPanels = styled.div`
+  background-color: green;
+  grid-area: other;
+`;
 const Statistics = () => {
-  return <div>Statistics</div>;
+  const { stats, loading } = useStats();
+
+  const isEmpty = stats === null;
+  if (loading) return <Loader />;
+
+  return (
+    <MainSection>
+      {isEmpty ? (
+        <p>There is no transaction</p>
+      ) : (
+        <SectionWrapper>
+          <Heading>Statistics</Heading>
+
+          <StatisticsWrapper>
+            <BalanceList {...stats} />
+
+            <Chart>CHart</Chart>
+
+            <CategoryBreakdown {...stats} />
+
+            <OtherPanels>OtherPanels</OtherPanels>
+          </StatisticsWrapper>
+        </SectionWrapper>
+      )}
+    </MainSection>
+  );
 };
 
 export default Statistics;
