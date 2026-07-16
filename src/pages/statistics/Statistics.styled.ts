4e3b2305cb716e0dd3f3bc2697cce6bff6689bd8
Albert <albertkepski3@gmail.com>
2026-07-16 10:24:16 +0200

    Update desktop breakpoint to bigDesktop in layout

--- src/pages/statistics/Statistics.styled.ts
+++ src/pages/statistics/Statistics.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StatisticsWrapper = styled.div`
   overflow-y: auto;
   max-height: 480px;
-  ${({ theme }) => theme.mq.tablet} {
+  ${({ theme }) => theme.mq.bigDesktop} {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-auto-rows: minmax(0, auto);
