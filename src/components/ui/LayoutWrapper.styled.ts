f7afdbed37a6e62d84e7233aa005dbc1ee4ade9e
Albert <albertkepski3@gmail.com>
2026-09-22 10:53:35 +0200

    Remove conditional max-width

--- src/components/ui/LayoutWrapper.styled.ts
+++ src/components/ui/LayoutWrapper.styled.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const LayoutWrapper = styled.div<{ $hasNavbar: boolean }>`
   ${({ theme }) => theme.mq.desktop} {
-    max-width: ${({ $hasNavbar }) => ($hasNavbar ? "1700px" : "auto")};
     display: flex;
     align-items: center;
     flex-direction: row-reverse;
